050d76339a211339e834c8526a43b3da9acdd2e7
RafiqAZ <zaini_ahmad29@yahoo.com>
2015-06-07 15:39:07 +0700

    tugaspd3

---
+++ Rafiqaz_XItkj2_tugaspd3.lpr
@@ -0,0 +1,21 @@
+program Rafiqaz_XItkj2_tugaspd3;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, Rafiqaz_XItkj2_tugaspd3_unit1, zcomponent
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
---
+++ rafiqaz_xitkj2_tugaspd3_unit1.pas
@@ -0,0 +1,38 @@
+unit Rafiqaz_XItkj2_tugaspd3_unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ZConnection;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ZConnection1: TZConnection;
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+end.
+
